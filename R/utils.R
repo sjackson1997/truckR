98f60366008c090bfc10a92a4cf585cc7c6d14eb
Jackson <KQV2764@krft.net>
2025-03-23 19:53:21 -0400

    Modified fix.ZIP() to keep space in Canadian postal codes.

--- R/utils.R
+++ R/utils.R
@@ -1,24 +1,35 @@
-fix.ZIP <- function(zip){
+fix.ZIP <- function(zip) {
   zip <- stringr::str_replace_all(zip, " ", "")
-  zip <- dplyr::case_when(zip=="926" ~ "PrRico",
-                         zip=="0" ~ "IMPEXP",
-                         stringr::str_length(zip) == 4 ~ stringr::str_c("0",zip),
-                         TRUE ~ zip)
+  zip <- dplyr::case_when(
+    zip == "926" ~ "PrRico",
+    zip == "0" ~ "IMPEXP",
+    stringr::str_length(zip) == 4 ~ stringr::str_c("0", zip),
+    stringr::str_detect(zip, "([:alpha:][:digit:]){3}") ~ str_replace(
+      zip,
+      "([:alpha:][:digit:][:alpha:])([:digit:][:alpha:][:digit:])",
+      "\\1 \\2"
+    ),
+    TRUE ~ zip
+  )
   return(zip)
 }
 
-fix.CTRY <- function(ctry){
-  ctry <-  dplyr::case_when(stringr::str_detect(ctry, "US")~ "USA",
-                          stringr::str_detect(ctry, "MX")~ "MEX",
-                          stringr::str_detect(ctry, "CA")~ "CAN",
-                          TRUE ~ ctry)
+fix.CTRY <- function(ctry) {
+  ctry <- dplyr::case_when(
+    stringr::str_detect(ctry, "US") ~ "USA",
+    stringr::str_detect(ctry, "MX") ~ "MEX",
+    stringr::str_detect(ctry, "CA") ~ "CAN",
+    TRUE ~ ctry
+  )
   return(ctry)
 }
 
-fix.MODE <- function(mode){
+fix.MODE <- function(mode) {
   mode <- stringr::str_to_upper(mode)
-  mode <- dplyr::case_when(stringr::str_detect(mode, "(OTR)|([^L]TL)")~ "TL",
-                          stringr::str_detect(mode, "^I")~"INTRMDL",
-                          TRUE ~ mode)
+  mode <- dplyr::case_when(
+    stringr::str_detect(mode, "(OTR)|([^L]TL)") ~ "TL",
+    stringr::str_detect(mode, "^I") ~ "INTRMDL",
+    TRUE ~ mode
+  )
   return(mode)
 }
