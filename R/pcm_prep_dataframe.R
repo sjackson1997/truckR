6c531541d3c8f460812dddc816038dcab7ae5729
Jackson <KQV2764@krft.net>
2025-07-02 12:22:24 -0400

    Updated pcm_prep_dataframe() and added documentation

--- R/pcm_prep_dataframe.R
+++ R/pcm_prep_dataframe.R
@@ -1,17 +1,26 @@
+#' Prepare dataframe for efficient use of PC Miler API
+#'
+#' @param df A dataframe that contains both origin and destination columns.
+#'
+#' @return A list containing prepared data for processing via PC Miler API.
+#' @export
+#'
+#' @examples
+#' pcm_prep_dataframe(df_route_samples)
+
 pcm_prep_dataframe <- function(df) {
   lst <- get_location_columns1(df)
   v_names <- lst$v_names
 
 
-
   df_labeled <- df %>%
-    arrange(!!!syms(v_names)) %>%
-    group_by(across(all_of(v_names))) %>%
-    mutate(
+    dplyr::arrange(!!!rlang::syms(v_names)) %>%
+    dplyr::group_by(dplyr::across(dplyr::all_of(v_names))) %>%
+    dplyr::mutate(
       RouteId = paste(
         "RTE",
-        str_pad(
-          cur_group_id(),
+        stringr::str_pad(
+          dplyr::cur_group_id(),
           width = ceiling(log(dim(.)[1], base = 10)) + 1,
           side = "left",
           pad = "0"
@@ -20,11 +29,11 @@ pcm_prep_dataframe <- function(df) {
       ),
       .before = 1,
     ) %>%
-    ungroup()
+    dplyr::ungroup()
 
   df_unique <- df_labeled %>%
-    group_by(RouteId, across(all_of(v_names))) %>%
-    summarise(n = n(), .groups = "drop")
+    dplyr::group_by(RouteId, dplyr::across(dplyr::all_of(v_names))) %>%
+    dplyr::summarise(n = dplyr::n(), .groups = "drop")
 
   lst_out <- list(
     "Labeled" = df_labeled,
